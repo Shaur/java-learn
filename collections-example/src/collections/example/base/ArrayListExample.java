708e0f84e6009914d5b59f9916d9737aedf2b1d0
Юрий Гламаздо <glamazdo.work@gmail.com>
2016-03-17 00:43:04 +0300

    added: примеры по ArrayList и LinkedList с комментариями

---
+++ collections-example/src/collections/example/base/ArrayListExample.java
@@ -0,0 +1,53 @@
+package collections.example.base;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * ArrayList реализует интерфейс List
+ * В основе лежит массив Object, в котором хранится значение
+ */
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        /**
+         * Создаем List с реализацией ArrayList
+         * Внутри хранятся значения типа String
+         */
+        List<String> list = new ArrayList<>();
+
+        /**
+         * Добавляем три значения
+         */
+        list.add("Fst");
+        list.add("Snd");
+        list.add("Thr");
+
+        /**
+         * Доступ к коллекции может быть реализован по индексу элемента
+         */
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        /**
+         * Так же можно использовать альтернативную версию for для перебора элементов
+         */
+        for (String value : list) {
+            System.out.println(value);
+        }
+
+        /**
+         * Удаление может происходить по индексу и по значению
+         */
+        list.remove(0);
+        list.remove("Snd");
+
+        /**
+         * Можно добавлять и удалять целыми коллекциями
+         */
+        list.addAll(Arrays.asList("Four", "Five", "Six"));
+        list.removeAll(Arrays.asList("Four", "Five"));
+    }
+}
---
+++ collections-example/src/collections/example/base/LinkedListExample.java
@@ -0,0 +1,57 @@
+package collections.example.base;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * LinkedList реализует интерфейс List
+ * В основе лежит двунаправленный список Node
+ * Внутри Node хранится значение и ссылки на следующий и предыдущий элементы
+ */
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+        /**
+         * Создаем List с реализацией LinkedList
+         * Внутри хранятся значения типа String
+         */
+        List<String> list = new LinkedList<>();
+
+        /**
+         * Добавляем три значения
+         */
+        list.add("Fst");
+        list.add("Snd");
+        list.add("Thr");
+
+        /**
+         * Доступ к коллекции может быть реализован по индексу элемента
+         * Важно! Стоит помнить, что это двусвязный список, а значит доступ по индексу
+         * эмулируется - на самом деле мы пробегаем от начала или от конца (в зависимости от индекса)
+         * на i элементов, где i - необходимый индекс
+         */
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        /**
+         * Так же можно использовать альтернативную версию for для перебора элементов
+         */
+        for (String value : list) {
+            System.out.println(value);
+        }
+
+        /**
+         * Удаление может происходить по индексу и по значению
+         */
+        list.remove(0);
+        list.remove("Snd");
+
+        /**
+         * Можно добавлять и удалять целыми коллекциями
+         */
+        list.addAll(Arrays.asList("Four", "Five", "Six"));
+        list.removeAll(Arrays.asList("Four", "Five"));
+    }
+}
